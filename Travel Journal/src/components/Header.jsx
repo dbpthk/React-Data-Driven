e3f87b058c31f82b6c55d0da045e5fb77e054914
Dhruv <dbpthk@gmail.com>
2025-05-19 00:13:23 +1000

    header section completed

---
+++ Travel Journal/src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="header">
+      <img
+        src="src/images/globe.png"
+        alt="globe logo"
+        className="header-logo"
+      />
+      <span>My Travel Journal</span>
+    </header>
+  );
+}
+export default Header;
